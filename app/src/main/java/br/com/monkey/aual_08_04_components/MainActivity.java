1ae9009f5bef34c63b2956addc17346b67086ed1
Ricardo Pulquerio <ricardo.pulquerio@unemat.br>
2024-04-15 19:36:53 -0400

    primeiro commit

---
+++ app/src/main/java/br/com/monkey/aual_08_04_components/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.monkey.aual_08_04_components;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
